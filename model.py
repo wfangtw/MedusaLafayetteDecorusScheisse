712394d3f02b0d836bdf2951143eda74d312d22a
peskotivesgeroff <b40815@gmail.com>
2015-10-06 18:44:10 +0800

    deleted myfunc.py

--- model.py
+++ model.py
@@ -1,11 +1,23 @@
 import numpy as np
 import theano
 import theano.tensor as T
-import myfunc as F
 
+#macro definitions
 INPUT_DIM = 39
 NEURONS_PER_LAYER = 256
 OUTPUT_DIM = 48
+
+#function definitions
+def ReLU(x):
+    return T.switch(x < 0, 0, x)
+def SoftMax(vec):
+    vec = T.exp(vec)
+    return vec / vec.sum()
+
+def Update(params, gradients):
+    mu = 0.05
+    param_updates = [ (p, p - mu * g) for p, g in zip(params, gradients) ]
+    return param_updates
 
 x = T.vector()
 y_hat = T.vector()
@@ -16,8 +28,8 @@ b = theano.shared(np.random.randn(OUTPUT_DIM))
 
 params = [W1, b1, W, b]
 
-a1 = F.ReLU(T.dot(W1,x) + b1)
-y = F.SoftMax( T.dot(W,a1) + b )
+a1 = ReLU(T.dot(W1,x) + b1)
+y = SoftMax( T.dot(W,a1) + b )
 
 cost = -T.log(y*y_hat).sum()
 
@@ -27,5 +39,5 @@ dparams = [dW1, db1, dW, db]
 output = theano.function(
         inputs=[x, y_hat],
         outputs=[y, cost],
-        updates = F.MyUpdate(params, dparams)
+        updates = Update(params, dparams)
         )
