09dbdf9338e425adf070879e4c060ae8316696d4
peskotivesgeroff <b40815@gmail.com>
2015-10-14 00:21:10 +0800

    Dataize for real

--- src/dataize/real/dataize.py
+++ src/dataize/real/dataize.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python
 import numpy as np
+import cPickle
 import random
 
-train_ip = []   # [ [39 dim], [39 dim], ... ]
-train_op = []   # [ [48 dim], [48 dim], ... ]
+train_ip = []   # [ [351 dim], [353 dim], ... ]
+train_op = []   # [ [1943 dim], [1943 dim], ... ]
 dev_ip = []     # data for validation
 dev_op = []     # data for validation
 
-map_48_39 = {}      # map 48 phones to 39 phones
-map_48_num = {}     # map 48 phones to index 0 ~ 47
+map_1943_39 = {}      # map 1943 phones to 39 phones
+
+label_dim = {}        # map label to id_list, id_list to dim_39[]
 
 # parse label/train.lab
-label = {}          # map label to phones
+label = {}          # map label to id_list, id_list to phomes
 label_male = {}
 label_female = {}
 label_dev_male = []
 label_dev_female = []
 
-f_label = open("/project/peskotiveswf/Workspace/MLDS_hw1/data/label/train.lab", "r")
+f_label = open("../../../data/state_label/train.lab", "r")
 
 for line in f_label:
     l = line.strip(' \n').split(',')
-    label[l[0]] = l[1]
     ll = l[0].split('_')
+    id_name = ll[0]+"_"+ll[1]
+
+    if id_name in label:                            # label data
+        label[id_name].append(l[1])
+    else:
+        label[id_name] = [l[1]]
+
     if ll[0][0] == 'm':                             # male data
         if ll[0] in label_male:
             if ll[1] not in label_male[ll[0]]:
@@ -52,51 +60,80 @@ label_dev = label_dev_male + label_dev_female
 
 # parse phones/48_39.map
 
-f_map = open("/project/peskotiveswf/Workspace/MLDS_hw1/data/phones/48_39.map", "r")
+f_map = open("../../../data/phones/state_48_39.map", "r")
 
 for index, line in enumerate(f_map):
     l = line.strip(' \n').split("\t")
-    map_48_39[l[0]] = l[1]
-    map_48_num[l[0]] = index
+    map_1943_39[l[0]] = l[2]
 f_map.close()
 
 # parse mfcc/train.ark
-f_mfcc = open("/project/peskotiveswf/Workspace/MLDS_hw1/data/mfcc/train.ark", "r")
+print "parse train.ark"
+f_mfcc = open("../../../data/mfcc/train.ark", "r")
 
 for line in f_mfcc:
     l = line.strip(' \n').split(' ')
-    dim_39 = l[1:]
-    dim_39 = map(float, dim_39)
-    dim_48 = [0] * 48
-    dim_48[map_48_num[label[l[0]]]] = 1
-    if l[0].split('_')[0] in label_dev:
-        dev_ip.append(dim_39)
-        dev_op.append(dim_48)
+    ll = l[0].split('_')
+    id_name = ll[0]+'_'+ll[1]
+
+    if id_name in label_dim:
+        label_dim[id_name].append(l[1:])
     else:
-        train_ip.append(dim_39)
-        train_op.append(dim_48)
+        label_dim[id_name] = [l[1:]]
 f_mfcc.close()
+print "aaa"
+
+train_ip = []
+
+for flabel in label_dim:
+    dim_351 = []
+    for x in range(6):
+        dim_351.extend(label_dim[flabel][0])
+    for x in range(3):
+        dim_351.extend(label_dim[flabel][1+x])
+    for x in range(len(label_dim[flabel])):
+        if x >= len(label_dim[flabel])-4:
+            dim_351.extend(label_dim[flabel][-1])
+        else:
+            dim_351.extend(label_dim[flabel][x+4])
+        for y in range(39):
+            dim_351.pop(0)
+
+        dim_351 = map(float, dim_351)
+        if flabel.split('_')[0] in label_dev:
+            dev_ip.append(dim_351)
+            dev_op.append(int(label[flabel][x])-1)
+        else:
+
+            train_ip.append(dim_351)
+            train_op.append(int(label[flabel][x])-1)
+
+
 
 # normalize train_ip
-train_ip = np.array(train_ip)
+print "normalize train_ip"
+train_ip = np.array(train_ip).astype(dtype=np.float64)
+print train_ip.dtype
 train_mean = np.mean(train_ip, axis=0, dtype=np.float64, keepdims=True)
 train_std = np.std(train_ip, axis=0, dtype=np.float64, ddof=1, keepdims=True)
 train_ip = (train_ip - train_mean) / train_std
 
 # normalize dev_ip
-dev_ip = np.array(dev_ip)
+print "normalize dev_ip"
+dev_ip = np.array(dev_ip).astype(dtype=np.float64)
+print dev_ip.dtype
 dev_mean = np.mean(dev_ip, axis=0, dtype=np.float64, keepdims=True)
 dev_std = np.std(dev_ip, axis=0, dtype=np.float64, ddof=1, keepdims=True)
 dev_ip = (dev_ip - dev_mean) / dev_std
 
 # write to file
+print "write to file"
 train = (train_ip.tolist(), train_op)
 dev = (dev_ip.tolist(), dev_op)
 
-f_train = open("/project/peskotiveswf/Workspace/MLDS_hw1/training_data/real/train.in", "w")
-f_train.write(str(train))
-f_train.close()
-
-f_dev = open("/project/peskotiveswf/Workspace/MLDS_hw1/training_data/real/dev.in", "w")
-f_dev.write(str(dev))
-f_dev.close()
+with open("../../../training_data/real/train.in", "w") as f_train:
+    #f_train.write(str(train))
+    cPickle.dump(train, f_train)
+with open("../../../training_data/real/dev.in", "w") as f_dev:
+    #f_dev.write(str(dev))
+    cPickle.dump(dev, f_dev)
