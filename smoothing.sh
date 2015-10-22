f3648a9252ab291e27680b95280e23b966429eb1
peskotivesgeroff <b40815@gmail.com>
2015-10-22 15:07:15 +0800

    modified script

--- smoothing.sh
+++ smoothing.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ $# -ne 2 ]; then
-    echo "Usage:smoothing.sh <input name(without suffix)> <output name(without suffix)>"
-	echo "ex: smoothing.sh myinput myoutput"
+    echo "Usage:smoothing.sh <input csv> <output csv>"
+	echo "ex: smoothing.sh predictions/myinput.csv predictions/myoutput.csv"
 	exit 1;
 fi
 
 pred_dir=predictions
 
-python2 -u src/smoothen.py $pred_dir/$1.csv $pred_dir/$2.csv
+python2 -u src/smoothen.py $1 $2
