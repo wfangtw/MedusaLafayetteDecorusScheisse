743141558ed1351756efa3ab766ab6f37076815c
peskotivesgeroff <b40815@gmail.com>
2015-11-04 19:40:08 +0800

    [Dropbox uploader for models, log, predictions, probabilities]

---
+++ hw1/sync.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: sync.sh <sync|download|upload>"
+	echo "ex: sync.sh upload"
+	exit 1;
+fi
+
+# script variable
+SCRIPT=~/Dropbox-Uploader/dropbox_uploader.sh
+
+# directory variable
+#DIRs=(predictions probabilities models log)
+DIRs=(test)
+
+if [ $1 == "upload" -o $1 == "download" ]; then
+    for dir in ${DIRs[@]}; do
+        $SCRIPT -sp $1 $dir .
+    done
+elif [ $1 == "sync" ]; then
+    for dir in ${DIRs[@]}; do
+        $SCRIPT -sp upload $dir .
+        $SCRIPT -sp download $dir .
+    done
+fi
